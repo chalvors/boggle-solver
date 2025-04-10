591cfcd45e73924d9629a755f20c05d0e9bd13c6
chalvors <darthcole28@gmail.com>
2025-04-10 13:27:36 -0700

    remove youtube stuff

--- backend/routes.py
+++ backend/routes.py
@@ -1,23 +1,7 @@
-from app import app, db
+from app import app
 from flask import request, jsonify
-from models import Friend
 import cv2
 import numpy as np
-
-
-# get all friends
-@app.route("/api/friends", methods=['GET'])
-def get_friends():
-    friends = Friend.query.all()
-    result = [friend.to_json() for friend in friends]
-    return jsonify(result)
-
-
-# create a friend
-@app.route("/api/friends", methods=['POST'])
-def create_friend():
-    return
-
 
 
 # solve boggle
