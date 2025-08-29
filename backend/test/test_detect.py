7b45bf6109b2f97539418e59276a908c4bdd2b3f
chalvors <darthcole28@gmail.com>
2025-08-29 14:55:18 -0700

    test detect board size

--- backend/test/test_detect.py
+++ backend/test/test_detect.py
@@ -3,6 +3,12 @@ sys.path.insert(1, '../')
 from detect import Detect
 import cv2
 import unittest
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="store_true", help="Show images for manual verification.")
+args = parser.parse_args()
 
 
 image = cv2.imread(r'./images/boggle_3.jpg')
@@ -12,8 +18,16 @@ board = detect.detect_letters(image)
 
 class TestImageDivision(unittest.TestCase):
 
-    def test_detect(self):
-        detect.print_board(board)
+    def test_detect_board_size(self):
+
+        np_arr = np.array(board)
+        self.assertEqual(np_arr.shape, (4,4))
+
+
+    def test_detect_print_board(self):
+
+        if (args.verbose):
+            detect.print_board(board)
 
 
 
