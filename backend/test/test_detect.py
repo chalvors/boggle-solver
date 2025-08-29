4c047c7a6152326ae03abe37984fbac7bb2b296a
chalvors <darthcole28@gmail.com>
2025-08-29 14:21:32 -0700

    detect test

--- backend/test/test_detect.py
+++ backend/test/test_detect.py
@@ -2,14 +2,20 @@ import sys
 sys.path.insert(1, '../')
 from detect import Detect
 import cv2
+import unittest
 
-def test_detect():
-    
-    image = cv2.imread(r'./images/boggle_3.jpg')
-    detect = Detect()
 
-    board = detect.detect_letters(image)
-    detect.print_board(board)
+image = cv2.imread(r'./images/boggle_3.jpg')
+detect = Detect()
+board = detect.detect_letters(image)
+
+
+class TestImageDivision(unittest.TestCase):
+
+    def test_detect(self):
+        detect.print_board(board)
+
+
 
 if __name__ == '__main__':
-    test_detect()
+    unittest.main()
