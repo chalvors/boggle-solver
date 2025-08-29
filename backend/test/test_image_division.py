7a44481d4250e6470e691208adf2e0b748632aa8
chalvors <darthcole28@gmail.com>
2025-08-29 13:58:45 -0700

    detect tests

--- backend/test/test_image_division.py
+++ backend/test/test_image_division.py
@@ -2,20 +2,47 @@ import sys
 sys.path.insert(1, '../')
 from detect import Detect
 import cv2
+import unittest
+import argparse
 
-def test_image_division():
-    image = cv2.imread(r'./images/boggle_3.jpg')
 
-    detect = Detect()
-    processedImage = detect.preprocess_image(image)
-    cells = detect.divide_image(processedImage)
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="store_true", help="Show images for manual verification.")
+args = parser.parse_args()
 
-    for cell in cells:
-        cv2.imshow("cell", cell)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+image = cv2.imread(r'./images/boggle_3.jpg')
+detect = Detect()
+processed_image = detect.preprocess_image(image)
+cell_images = detect.divide_image(processed_image)
+
+
+class TestImageDivision(unittest.TestCase):
+    
+    def test_image_division_cell_count(self):
+
+        self.assertEqual(len(cell_images), 16)
+
+    
+    def test_image_division_cells_are_square(self):
+
+        cell_shapes = []
+
+        for cell_image in cell_images:
+            cell_shapes.append(cell_image.shape)
+        
+        self.assertTrue(bool(len(set(cell_shapes)))) # Shapes of all cells are the same
+
+    
+    def test_image_division_show_cells(self):
+
+        if (args.verbose):
+
+            for cell_image in cell_images:
+                cv2.imshow("cell", cell_image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
 
 
 
 if __name__ == '__main__':
-    test_image_division()
+    unittest.main()
--- backend/test/test_image_preprocessing.py
+++ backend/test/test_image_preprocessing.py
@@ -2,19 +2,46 @@ import sys
 sys.path.insert(1, '../')
 from detect import Detect
 import cv2
+import unittest
+import argparse
 
 
-def test_image_preprocessing():
-    image = cv2.imread(r'./images/boggle_3.jpg')
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="store_true", help="Show images for manual verification.")
+args = parser.parse_args()
 
-    detect = Detect()
-    processedImage = detect.preprocess_image(image)
 
-    cv2.imshow("input", image)
-    cv2.imshow("result", processedImage)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+image = cv2.imread(r'./images/boggle_2.jpg')
+detect = Detect()
+processed_image = detect.preprocess_image(image)
+
+
+class TestImagePreprocessing(unittest.TestCase):
+
+    def test_image_preprocessing_grayscale(self):
+
+        if processed_image.ndim == 2:
+            pass
+        else:
+            raise AssertionError("processed image is not grayscale")
+        
+
+    def test_image_preprocessing_square(self):
+
+        (processed_image_height, processed_image_width) = processed_image.shape
+        self.assertEqual(processed_image_height, processed_image_width)  # Processed image is a square
+
+    
+    def test_image_preprocessing_show_before_and_after(self):
+
+        if (args.verbose):
+            cv2.imshow("input", image)
+            cv2.imshow("result", processed_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+
 
 if __name__ == '__main__':
-    test_image_preprocessing()
+    unittest.main()
 
